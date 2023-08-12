905d6acec35ac7bcde6d29fb42ad178b74d75df0
Ben <equa.ferrous15@gmail.com>
2023-08-12 20:21:26 +0100

    Added the Customer List Screen
    - Created CustomerListScreen.
    - Created CustomerListEntry to populate the list.
    - Added a general forward arrow image.
    - Updated main.py to show the customer list screen on startup.

--- papasGuideria/main.py
+++ papasGuideria/main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from ui.MainWindow import MainWindow
+from ui.customerlist.CustomerListScreen import CustomerListScreen
 
 
 if __name__ == "__main__":
@@ -8,6 +9,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     window = MainWindow()
+    window.show_new_screen(CustomerListScreen())
 
     window.show()
     sys.exit(app.exec())
