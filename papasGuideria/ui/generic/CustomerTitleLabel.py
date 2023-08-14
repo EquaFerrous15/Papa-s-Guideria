c24592e994124372351a5359a43bfdc95e66df31
EquaFerrous15 <141756369+EquaFerrous15@users.noreply.github.com>
2023-08-14 15:34:42 +0100

    Changed CustomerTitleLabel to inherit QLabel
    - Switch CustomerTitleLabel from inheriting AbstractCustomWidget to QLabel.
    - This allows easier control over the label text and style.

--- papasGuideria/ui/generic/CustomerTitleLabel.py
+++ papasGuideria/ui/generic/CustomerTitleLabel.py
@@ -3,37 +3,35 @@ from PyQt5.QtWidgets import QLabel, QGridLayout
 from .AbstractCustomWidget import AbstractCustomWidget
 
 
-class CustomerTitleLabel(AbstractCustomWidget):
+class CustomerTitleLabel(QLabel):
     """A label to display a customer's title with special formatting depending on the title."""
 
     def __init__(self, customer_title: str, parent=None):
-        super().__init__(parent, customer_title)
+        super().__init__(parent)
 
-    def create_ui(self, customer_title: str):
         if customer_title is None:
             customer_title = "Normal Customer"
         customer_title = customer_title.title()
 
         # Choose text colour
-        text_colour = "color: "
         match customer_title:
             case "Food Critic":
-                text_colour += "'#0167CD';"
+                text_colour = "#0167CD"
             case "Closer" | "Ex-Closer":
-                text_colour += "'#CD0101';"
+                text_colour = "#CD0101"
             case "Local":
-                text_colour += "'#FEC418';"
+                text_colour = "#FEC418"
             case _:
-                text_colour += "'#CBCBCB';"
-
-        # Main layout
-        layout = QGridLayout(self)
-        layout.setContentsMargins(0, 0, 0, 0)
+                text_colour = "#CBCBCB"
+        self._text_style = f"color: '{text_colour}';"
 
         # Title label
-        title_label = QLabel(customer_title)
-        title_label.setStyleSheet(
-            "font: 30px;" +
-            text_colour
+        self.setText(customer_title)
+        self.setStyleSheet(
+            self._text_style
         )
-        layout.addWidget(title_label, 0, 0)
+
+    def setStyleSheet(self, p_str):
+        style_string = self._text_style + p_str
+        super().setStyleSheet(style_string)
+
