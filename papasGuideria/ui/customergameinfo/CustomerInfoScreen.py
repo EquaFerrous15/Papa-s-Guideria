61bbed8aedf3bf4ee72adca330cdb074087b0f4c
EquaFerrous15 <141756369+EquaFerrous15@users.noreply.github.com>
2023-08-14 18:59:42 +0100

    CustomerInfoScreen added CustomerHeader
    - Changed colour of default CustomerHeader title text

--- papasGuideria/ui/customergameinfo/CustomerInfoScreen.py
+++ papasGuideria/ui/customergameinfo/CustomerInfoScreen.py
@@ -1,7 +1,9 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QGridLayout
 
 from ..generic.AbstractScreen import AbstractScreen
 from ..generic.CustomImageLabel import CustomImageLabel
+from ..generic.CustomerNameHeader import CustomerNameHeader
 from ...data.Customer import Customer
 from ...data.Game import Game
 
@@ -20,3 +22,7 @@ class CustomerInfoScreen(AbstractScreen):
         game_portrait = CustomImageLabel(customer.game_info[game.name]["Portrait"])
         game_portrait.resize_by_scale_factor(3)
         layout.addWidget(game_portrait, 0, 0, -1, 1)
+
+        # Customer header
+        customer_header = CustomerNameHeader(customer.name, customer.game_info[game.name]["Title"])
+        layout.addWidget(customer_header, 0, 1, 1, -1, Qt.AlignTop)
