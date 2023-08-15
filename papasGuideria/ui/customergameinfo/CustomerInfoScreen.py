2e7928ed7e853b97aafb9c777630f75439a9e696
EquaFerrous15 <141756369+EquaFerrous15@users.noreply.github.com>
2023-08-15 18:23:15 +0100

    Info screen changed to box layouts
    - Changed customer info screen to use box layouts instead of a grid so it looks correct.

--- papasGuideria/ui/customergameinfo/CustomerInfoScreen.py
+++ papasGuideria/ui/customergameinfo/CustomerInfoScreen.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QGridLayout, QSizePolicy
+from PyQt5.QtWidgets import QGridLayout, QSizePolicy, QVBoxLayout, QHBoxLayout, QSpacerItem
 
 from .CustomerInfoWidget import CustomerInfoWidget
 from ..generic.AbstractScreen import AbstractScreen
@@ -17,16 +17,30 @@ class CustomerInfoScreen(AbstractScreen):
 
     def create_ui(self, customer: Customer, game: Game):
         # Main layout
-        layout = QGridLayout(self)
+        layout = QHBoxLayout(self)
+
+        # Left layout
+        left_layout = QVBoxLayout()
+        layout.addLayout(left_layout)
 
         # Customer game portrait
         game_portrait = CustomImageLabel(customer.game_info[game.name]["Portrait"])
         game_portrait.resize_by_scale_factor(3)
-        layout.addWidget(game_portrait, 0, 0)
+        left_layout.addWidget(game_portrait)
+
+        # Order ticket image
+        order_ticket = CustomImageLabel(customer.game_info[game.name]["Order"])
+        order_ticket.resize_by_scale_factor(1.1)
+        left_layout.addWidget(order_ticket, alignment=Qt.AlignHCenter)
+
+        # Right layout
+        right_layout = QVBoxLayout()
+        layout.addLayout(right_layout)
 
         # Customer header
         customer_header = CustomerNameHeader(customer.name, customer.game_info[game.name]["Title"])
-        layout.addWidget(customer_header, 0, 1, 1, -1, Qt.AlignTop | Qt.AlignHCenter)
+        customer_header.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Fixed)
+        right_layout.addWidget(customer_header)
 
         # Customer info section
         customer_info = CustomerInfoWidget(customer, game)
@@ -34,9 +48,9 @@ class CustomerInfoScreen(AbstractScreen):
         customer_info.setStyleSheet(
             "margin: 10px 30px 0px 30px;"
         )
-        layout.addWidget(customer_info, 1, 1, Qt.AlignTop)
+        right_layout.addWidget(customer_info)
 
-        # Order ticket image
-        order_ticket = CustomImageLabel(customer.game_info[game.name]["Order"])
-        order_ticket.resize_by_scale_factor(1.1)
-        layout.addWidget(order_ticket, 1, 0, Qt.AlignHCenter)
+        # Right spacer moves everything up
+        right_layout.addSpacerItem(QSpacerItem(1, 1, QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding))
+
+
